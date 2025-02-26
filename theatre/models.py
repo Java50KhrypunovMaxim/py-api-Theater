b28510f872b8c93a28f839a14e89d3610d333f6b
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-02-26 17:07:12 +0200

    Add models

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,90 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Play(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    actor = models.ManyToManyField("Actor", related_name="plays",
+                                   blank=True)
+    genre = models.ManyToManyField("Genre", related_name="plays",
+                                   blank=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Performance(models.Model):
+    play = models.ForeignKey("Play", on_delete=models.CASCADE)
+    theatre_hall = models.ForeignKey("TheatreHall", on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self):
+        return str(self.id)
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    id = models.IntegerField(primary_key=True)
+    created_at = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey("Performance", on_delete=models.CASCADE)
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def _str_(self):
+        return (
+            f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
