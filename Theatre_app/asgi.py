8d9d1be4ecbcdabaace617c1b9d61e8ea6fd3ac0
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-02-26 13:56:24 +0200

    Add requirements

--- Theatre_app/asgi.py
+++ Theatre_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Theatre project.
+ASGI config for Theatre_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Theatre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Theatre_app.settings')
 
 application = get_asgi_application()
--- Theatre_app/wsgi.py
+++ Theatre_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Theatre project.
+WSGI config for Theatre_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Theatre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Theatre_app.settings')
 
 application = get_wsgi_application()
