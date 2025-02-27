e9b8105bc4b6ea76a28ee85848ea792b2163cce7
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-02-27 09:54:00 +0200

    Add superuser and db to gitignore

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from theatre.models import Genre, Actor, Performance, Play, Ticket, TheatreHall, Reservation
+
+admin.site.register(Performance)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Play)
+admin.site.register(Ticket)
+admin.site.register(TheatreHall)
+admin.site.register(Reservation)
+
+
+
