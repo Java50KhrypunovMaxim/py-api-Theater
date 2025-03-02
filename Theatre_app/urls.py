6ad0ebc8dbf463581fcc9a813cf288f6f696cc6c
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-03-02 15:00:17 +0200

    add user Authentication (token)

--- Theatre_app/urls.py
+++ Theatre_app/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from django.contrib import admin
 from django.urls import path, include
@@ -24,6 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="cinema")),
+    path("api/user/", include("user.urls", namespace="user")),
     path('api/doc/', SpectacularAPIView.as_view(), name="schema"),
     path('api/doc/swagger/', SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from user.views import CreateUserView, CreateTokenView, ManageUserView
+from rest_framework.authtoken import views
 
-app_name = "user"
+from user.views import CreateUserView, LoginUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
+    path("login/", LoginUserView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
+
+app_name = "user"
