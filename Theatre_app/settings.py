c09d148b3e259e26ef123472422909d997059357
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-03-13 18:35:02 +0200

    add docker-compose.yaml

--- Theatre_app/settings.py
+++ Theatre_app/settings.py
@@ -63,9 +63,13 @@ WSGI_APPLICATION = 'Theatre_app.wsgi.application'
 
 
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
 
