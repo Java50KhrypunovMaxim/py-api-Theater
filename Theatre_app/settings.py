4cbef8c5abcb34e8e2c202735f3e0dcf2ace69c0
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-03-14 19:58:21 +0200

    add postgres volumes to doc-compose

--- Theatre_app/settings.py
+++ Theatre_app/settings.py
@@ -60,15 +60,16 @@ WSGI_APPLICATION = 'Theatre_app.wsgi.application'
 
 
 DATABASES = {
-     "default": {
+    "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
